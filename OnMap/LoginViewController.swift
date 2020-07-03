21bd2c750c79c22898fa14884e934ceec3c47e49
Sarah A <sam1590paddock@yahoo.com>
2020-07-03 13:09:59 +0300

    Add Layout to Login View Add auto layout to match portrait and landscape views on all devices for Login View

--- OnMap/LoginViewController.swift
+++ OnMap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  OnMap
 //
 //  Created by Sarah Al-Matawah on 30/06/2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ OnMap/PostInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostInfoViewController.swift
+//  OnMap
+//
+//  Created by Sarah Al-Matawah on 03/07/2020.
+//  Copyright © 2020 Sarah Al-Matawah. All rights reserved.
+//
+
+import UIKit
+
+class PostInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
